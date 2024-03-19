0658f0643fdc553a9355eae05e82ce5f1e25d3c5
Binye Barwe <binyebarwe@gmail.com>
2024-03-19 14:07:24 -0700

    new landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./logo.png";
+import { FaTwitter, FaGithub } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="start-bg flex flex-col justify-center items-center h-screen">
+      {/* Responsive Logo */}
+      <img
+        src={logo}
+        alt="Logo"
+        className="fixed top-10 w-20 left-5 sm:left-10 sm:p-2 sm:w-30 sm:h-30 md:left-20 md:w-45 md:h-35 "
+      />
+
+      {/* Content */}
+      <div className="text-white text-center">
+        <h1 className="text-4xl sm:text-3xl md:text-5xl font-bold">
+          Plata Markets
+        </h1>
+        <br />
+        <br />
+        <p className="text-lg sm:text-base md:text-xl">
+          A DEX built for Market Makers
         </p>
+      </div>
+
+      {/* Footer */}
+      <footer className="fixed bottom-0 w-full py-4 text-white text-sm flex items-center justify-center mb-8">
+        <h3 className="text-base sm:text-base md:text-base mr-9">
+          © 2024 PlataMarkets. All Rights Reserved.
+        </h3>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://twitter.com/platamarkets"
+          className="hover:underline mx-2"
         >
-          Learn React
+          <FaTwitter className="inline-block mr-2 text-2xl" />
         </a>
-      </header>
+        <a
+          href="https://www.github.com/platamarkets"
+          className="hover:underline mx-2"
+        >
+          <FaGithub className="inline-block mr-2  text-2xl" />
+        </a>
+      </footer>
     </div>
   );
 }
